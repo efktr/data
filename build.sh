911b6c739db6727ab199403af0d23c4f1a189b87
Christian Dallago <code@dallago.us>
2017-03-10 17:25:55 +0100

    added messages

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 echo "Running python scripts to collect data and fill the database"
 
+echo "Collecting sider data"
 python sider.py
+
+echo "Collecting drugbank data to create drugbank-pubchem mapping"
 python drugbanckToPubChem.py
+
+echo "Collecting drugbank data"
 python drugbank.py
 
 exit 0
