bf27639dfa80cddb21b9e6bb040e16c45a6b6f2a
Christian Dallago <code@dallago.us>
2017-09-09 17:59:24 +0200

    fixed multiple ids

--- drugbank.py
+++ drugbank.py
@@ -80,9 +80,6 @@ with open(os.path.join(temp_folder, scope_name, "full database.xml"), 'rb') as f
             if isinstance(drug['drugbank-id'], list):
                 current['drugbankId'] = [e['#text'] for e in drug['drugbank-id'] if isinstance(e, dict) and e['@primary'] == 'true'][0]
                 current['otherIds'] = [e for e in drug['drugbank-id'] if isinstance(e, str)]
-
-                print(current)
-                quit()
             else:
                 current['drugbankId'] = drug['drugbank-id']['#text']
 
