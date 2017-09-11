0f32dbd20d5ee49fb23b99840206c93cefd1c519
Christian Dallago <code@dallago.us>
2017-09-11 13:27:33 +0200

    new example queries

--- sql/_example_queries.sql
+++ sql/_example_queries.sql
@@ -15,33 +15,31 @@ ORDER BY support DESC;
 -- Find all drugs and products that give an ADR in > 50% of cases
 SELECT drugbank.drugbank_id, drugbank.name, json_agg(joint_result.product) AS products
 FROM drugbank
-JOIN (
-    SELECT
-      drugbank_products.product,
-      drugbank_products.drugbank_id
-    FROM drugbank_products,
-      (
-        SELECT DISTINCT (drugbank_id)
-        FROM pubchem_to_drugbank,
-          (
-            SELECT DISTINCT (adverse_drug_reactions.pubchem_id)
-            FROM adverse_drug_reactions,
-              (
-                SELECT DISTINCT (umls_id)
-                FROM umls_dictionary
-                WHERE umls_dictionary.name ~* '^headache'
-              ) AS interesting_adrs
-            WHERE adverse_drug_reactions.umls_id = interesting_adrs.umls_id AND
-                  NUMRANGE(0.5, 1.0) @> adverse_drug_reactions.range
-          ) AS interesting_ards_pubchem
-        WHERE interesting_ards_pubchem.pubchem_id = pubchem_to_drugbank.pubchem_id
-      ) AS interesting_db
-    WHERE drugbank_products.drugbank_id = interesting_db.drugbank_id
-    ) AS joint_result
-  ON joint_result.drugbank_id = drugbank.drugbank_id
+  JOIN (
+         SELECT
+           drugbank_products.product,
+           drugbank_products.drugbank_id
+         FROM drugbank_products,
+           (
+             SELECT DISTINCT (drugbank_id)
+             FROM pubchem_to_drugbank,
+               (
+                 SELECT DISTINCT (adverse_drug_reactions.pubchem_id)
+                 FROM adverse_drug_reactions,
+                   (
+                     SELECT DISTINCT (umls_id)
+                     FROM umls_dictionary
+                     WHERE umls_dictionary.name ~* '^headache'
+                   ) AS interesting_adrs
+                 WHERE adverse_drug_reactions.umls_id = interesting_adrs.umls_id AND
+                       NUMRANGE(0.5, 1.0) @> adverse_drug_reactions.range
+               ) AS interesting_ards_pubchem
+             WHERE interesting_ards_pubchem.pubchem_id = pubchem_to_drugbank.pubchem_id
+           ) AS interesting_db
+         WHERE drugbank_products.drugbank_id = interesting_db.drugbank_id
+       ) AS joint_result
+    ON joint_result.drugbank_id = drugbank.drugbank_id
 GROUP BY drugbank.drugbank_id;
-
-
 
 -- High confidence ADRs --> can be considered trivial?
 SELECT umls_dictionary.umls_id, umls_dictionary.name
@@ -86,16 +84,70 @@ FROM
   (
     SELECT drugbank_id, json_agg(product) AS products
     FROM drugbank_products
-    WHERE drugbank_id = 'DB00001'
+    WHERE drugbank_id = (
+      SELECT drugbank_id
+      FROM drugbank_id_mapping
+      WHERE mapping = 'DB00001'
+      LIMIT 1
+    )
     GROUP BY drugbank_id
   ) AS dbp,
   (
     SELECT drugbank_id, json_agg(synonym) AS synonyms
     FROM drugbank_synonyms
-    WHERE drugbank_id = 'DB00001'
+    WHERE drugbank_id = (
+      SELECT drugbank_id
+      FROM drugbank_id_mapping
+      WHERE mapping = 'DB00001'
+      LIMIT 1
+    )
     GROUP BY drugbank_id
   ) AS dbs
 WHERE
-  db.drugbank_id = 'DB00001' AND
+  db.drugbank_id = (
+    SELECT drugbank_id
+    FROM drugbank_id_mapping
+    WHERE mapping = 'DB00001'
+    LIMIT 1
+  ) AND
   db.drugbank_id = dbp.drugbank_id AND
   dbp.drugbank_id = dbs.drugbank_id;
+
+
+-- Get ADRs for drug:
+
+SELECT dict.umls_id, dict.name, adrs.lower, adrs.higher
+FROM umls_dictionary as dict
+  JOIN (
+         SELECT DISTINCT
+           umls_id,
+           AVG(lower) as lower,
+           AVG(higher) as higher
+         FROM stitch_to_umls
+         WHERE stitch_id = (
+           SELECT stitch_id
+           FROM stitch_to_drugbank
+           WHERE drugbank_id = (
+             SELECT drugbank_id
+             FROM drugbank_id_mapping
+             WHERE mapping = 'APRD00417'
+             LIMIT 1
+           )
+           LIMIT 1) GROUP BY umls_id
+       ) AS adrs
+    ON adrs.umls_id = dict.umls_id;
+
+-- Get drugs by ADR
+SELECT db.drugbank_id, db.name
+FROM drugbank AS db
+  JOIN (
+         SELECT drugbank_id
+         FROM stitch_to_drugbank AS std
+           JOIN (
+                  SELECT DISTINCT stitch_id
+                  FROM stitch_to_umls
+                  WHERE umls_id = 'C0596240'
+                ) AS stitch_mapping
+             ON stitch_mapping.stitch_id = std.stitch_id
+       ) AS drugbank_mapping
+    ON drugbank_mapping.drugbank_id = db.drugbank_id;
